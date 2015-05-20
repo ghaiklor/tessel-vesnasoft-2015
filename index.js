d8219901fd822ddfc91c94062fe9dcc577a439dd
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-20 20:01:16 +0300

    Done with Ambient wrapper

--- index.js
+++ index.js
@@ -3,41 +3,30 @@
 var tessel = require('tessel');
 
 var Camera = require('./lib/Camera');
+var Ambient = require('./lib/Ambient');
+
 var camera = new Camera('A', {
   compression: 0.2,
   resolution: 'vga'
 });
 
-// var ambient = require('ambient-attx4').use(tessel.port.B);
+var ambient = new Ambient('B');
+
 // var accelerometer = require('accel-mma84').use(tessel.port.C);
 // var climate = require('climate-si7020').use(tessel.port.D);
 
 var notificationLED = tessel.led[1].output(0);
-camera.on('ready', function () {
-  notificationLED.output(1);
-  camera.takePicture().then(function (image) {
-    process.sendfile(Date.now() + '.jpg', image);
-  }).catch(console.error.bind(console.error));
-});
+//camera.on('ready', function () {
+//  notificationLED.output(1);
+//  camera.takePicture().then(function (image) {
+//    process.sendfile(Date.now() + '.jpg', image);
+//  }).catch(console.error.bind(console.error));
+//});
 
-// ambient.on('ready', function() {
-//   notificationLED.output(1);
-//
-//   ambient.setSoundTrigger(0.1);
-//   ambient.on('sound-trigger', function() {
-//     ambient.clearSoundTrigger();
-//
-//     camera.takePicture(function(error, image) {
-//       if (error) console.error(error);
-//
-//       var name = 'picture-' + Date.now() + '.jpg';
-//       console.log('Saving as ' + name);
-//       process.sendfile(name, image);
-//
-//       setTimeout(ambient.setSoundTrigger.bind(ambient, 0.1), 5000);
-//     });
-//   });
-// });
+ambient.on('ready', function () {
+  notificationLED.output(1);
+  ambient.getNativeModule().on('light', console.log.bind(console.log));
+});
 //
 // ambient.on('error', console.error.bind(console.error));
 // camera.on('error', console.error.bind(console.error));
