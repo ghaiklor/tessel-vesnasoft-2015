68c74f483c2cc2b659c4439299a760563828ff4d
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-20 18:18:46 +0300

    Done with Camera wrapper and promises

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+'use strict';
+
 var tessel = require('tessel');
-var camera = require('camera-vc0706').use(tessel.port['A']);
-var ambient = require('ambient-attx4').use(tessel.port['B']);
 
-var notificationLED = tessel.led[1].output(0);
-
-ambient.on('ready', function() {
-  notificationLED.output(1);
-
-  ambient.setSoundTrigger(0.1);
-  ambient.on('sound-trigger', function() {
-    ambient.clearSoundTrigger();
-
-    camera.takePicture(function(error, image) {
-      if (error) console.error(error);
-
-      var name = 'picture-' + Date.now() + '.jpg';
-      console.log('Saving as ' + name);
-      process.sendfile(name, image);
-
-      setTimeout(ambient.setSoundTrigger.bind(ambient, 0.1), 5000);
-    });
-  });
+var Camera = require('./lib/Camera');
+var camera = new Camera({
+  port: 'A',
+  compression: 0.2,
+  resolution: 'vga'
 });
 
-ambient.on('error', console.error.bind(console.error));
-camera.on('error', console.error.bind(console.error));
+// var ambient = require('ambient-attx4').use(tessel.port.B);
+// var accelerometer = require('accel-mma84').use(tessel.port.C);
+// var climate = require('climate-si7020').use(tessel.port.D);
+
+var notificationLED = tessel.led[1].output(0);
+camera.on('ready', function() {
+  notificationLED.output(1);
+  camera.takePicture().then(function(image) {
+    process.sendfile(Date.now() + '.jpg', image);
+  }).catch(console.error.bind(console.error));
+});
+
+// ambient.on('ready', function() {
+//   notificationLED.output(1);
+//
+//   ambient.setSoundTrigger(0.1);
+//   ambient.on('sound-trigger', function() {
+//     ambient.clearSoundTrigger();
+//
+//     camera.takePicture(function(error, image) {
+//       if (error) console.error(error);
+//
+//       var name = 'picture-' + Date.now() + '.jpg';
+//       console.log('Saving as ' + name);
+//       process.sendfile(name, image);
+//
+//       setTimeout(ambient.setSoundTrigger.bind(ambient, 0.1), 5000);
+//     });
+//   });
+// });
+//
+// ambient.on('error', console.error.bind(console.error));
+// camera.on('error', console.error.bind(console.error));
