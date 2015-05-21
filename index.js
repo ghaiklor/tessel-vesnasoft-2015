e9bd347ba2b54659382a44bfd0438bb7201d93ed
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-21 16:15:01 +0300

    Done with ModuleLoader

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 /**
  * Wrappers for tessel modules
  */
-var Accelerometer = require('./lib/modules/AccelerometerModule');
-var Ambient = require('./lib/modules/AmbientModule');
-var Camera = require('./lib/modules/CameraModule');
-var ClimateModule = require('./lib/modules/ClimateModule');
+var ModuleLoader = require('./lib/ModuleLoader');
 var LEDModule = require('./lib/modules/LEDModule');
+var led = new LEDModule().startAnimation();
 
-new LEDModule().startAnimation();
+/**
+ * Triggers when all modules is ready to work
+ * @param {Object} modules
+ * @private
+ */
+function _onModulesReady(modules) {
+  modules.Accelerometer.getNativeModule().on('data', console.log.bind(console.log));
+  setTimeout(led.stopAnimation.bind(led), 2000);
+}
+
+new ModuleLoader({
+  Accelerometer: 'C',
+  Ambient: 'B',
+  Camera: 'A',
+  Climate: 'D'
+}).on('ready', _onModulesReady);
