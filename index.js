2667fdbc69ce2d0d665ce4c21c674dff7e131ffb
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-21 15:30:07 +0300

    Done with animation in LEDModule

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+/**
+ * Wrappers for tessel modules
+ */
 var Accelerometer = require('./lib/modules/AccelerometerModule');
 var Ambient = require('./lib/modules/AmbientModule');
 var Camera = require('./lib/modules/CameraModule');
 var ClimateModule = require('./lib/modules/ClimateModule');
 var LEDModule = require('./lib/modules/LEDModule');
 
-var lights = new LEDModule({
-  LED1: true,
-  CONN: true
-});
+new LEDModule().startAnimation();
