5acdf484c4344153d7750fd49bf1da3684d626b4
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-22 13:20:04 +0300

    Improve sending data via server

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ var LEDModule = require('./lib/modules/LEDModule');
 var ModuleLoader = require('./lib/ModuleLoader');
 var WebSocketServer = require('./lib/WebSocketServer');
 var led = new LEDModule();
-//var server = new WebSocketServer(3000);
+var server = new WebSocketServer(3000);
+
+/**
+ * Triggers when accelerometer data is received
+ * @param data
+ * @private
+ */
+function _onAccelerometerData(data) {
+  server.send({
+    type: 'accelerometer',
+    data: data
+  });
+}
+
+/**
+ * Triggers when ambient sound level is triggered
+ * @param data
+ * @private
+ */
+function _onAmbientSoundTrigger(data) {
+  server.send({
+    type: 'restart',
+    data: data
+  });
+}
 
 /**
  * Triggers when all modules is ready to work
@@ -10,11 +34,13 @@ var led = new LEDModule();
  * @private
  */
 function _onModulesReady(modules) {
-  led.startAnimation();
-  modules.Servo.startRotating(1);
-  //modules.Accelerometer.getNativeModule().on('data', server.send.bind(server));
+  modules.Accelerometer.getNativeModule().on('data', _onAccelerometerData);
+  modules.Ambient.onSoundTrigger(0.2, 1000, _onAmbientSoundTrigger);
+
+  led.on('LED2');
 }
 
 new ModuleLoader({
-  Servo: 'A'
+  Accelerometer: 'A',
+  Ambient: 'C'
 }).on('ready', _onModulesReady);
