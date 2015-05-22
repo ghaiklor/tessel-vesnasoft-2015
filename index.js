be5d81d8639410a7fb0ca84caa017ab4c9a287db
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-22 19:25:36 +0300

    Add cheat to the game

--- index.js
+++ index.js
@@ -25,10 +25,21 @@ function _onAccelerometerData(data) {
  * @param data
  * @private
  */
-function _onClapTrigger(data) {
+function _onRestartGame(data) {
   server.send({
     type: 'restart',
     data: data
+  });
+}
+
+/**
+ * Triggers when cheat is activated
+ * @private
+ */
+function _onCheatActivated() {
+  server.send({
+    type: 'cheat',
+    data: {}
   });
 }
 
@@ -39,7 +50,8 @@ function _onClapTrigger(data) {
  */
 function _onModulesReady(modules) {
   modules.Accelerometer.getNativeModule().on('data', _onAccelerometerData);
-  modules.Ambient.onClapTrigger(_onClapTrigger);
+  modules.Ambient.onClapTrigger(_onRestartGame, 2);
+  modules.Ambient.onClapTrigger(_onCheatActivated, 1);
   led.on('LED2');
 }
 
